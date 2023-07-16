9e4ed5e9fcd431ef9a64c527be9b657ef5e76e71
th3mon <p.szelenberger@gmail.com>
2023-07-16 03:44:31 +0200

    Add useful keymaps created by Primeagen

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,18 @@
 -- Add any additional keymaps here
 -- TODO: Rename pv to maybe fv -> _F_older _V_iew
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Go to file exploration" })
+
+-- Move highlighted lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Joining lines but cursors stays
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Moving up or down with cursor in the middle of the screen
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Search with cursor in the middle of the screen
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")

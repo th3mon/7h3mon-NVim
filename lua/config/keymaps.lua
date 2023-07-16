b425adb79a0dab0fa6ecf7844fc25e1f53e7b75d
th3mon <p.szelenberger@gmail.com>
2023-07-16 02:36:22 +0200

    Add <leader>pv keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- TODO: Rename pv to maybe fv -> _F_older _V_iew
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Go to file exploration" })

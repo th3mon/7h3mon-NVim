c615520e75833efd9498a4f7fb721bd8c8b4df45
th3mon <p.szelenberger@gmail.com>
2023-08-03 00:53:13 +0200

    Fix typo

--- lua/plugins/treesitter-playground.lua
+++ lua/plugins/treesitter-playground.lua
@@ -1,5 +1,5 @@
 -- INFO: TreeSitter Playground is usefull when you develop NeoVim Plugin
--- To sho it go :TSPlaygroundToggle
+-- To show it go :TSPlaygroundToggle
 return {
   "nvim-treesitter/playground",
 }
